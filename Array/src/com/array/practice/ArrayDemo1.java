c323ae16009ebd817f6a2652472cb9d5d12385e7
Aniket9309 <aniketchatare7@gmail.com>
2022-09-21 00:02:38 +0530

    adding array deo

---
+++ Array/src/com/array/practice/ArrayDemo1.java
@@ -0,0 +1,25 @@
+package com.array.practice;
+
+public class ArrayDemo1 {
+	
+	public static void main(String[]args) {
+		
+		int[] a=new int[5];
+		//int a=10;
+		a[1]=20;
+		a[2]=30;
+		a[3]=40;
+		a[4]=50;
+		//a[5]=60;
+		
+		for(int i=0;i<a.length;i++) 
+			
+			System.out.println(a[i]);
+			
+		
+		
+		
+		
+	}
+
+}
